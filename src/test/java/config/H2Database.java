471a714d44ca393dbfcc244c32ccd65669603389
ograh <bernard.ograh@amalitech.com>
2026-01-05 10:45:19 +0000

    feat: Add H2Database and H2DatabaseConfig classes for improved database connection handling; update DatabaseTest to include H2 connection tests

--- src/test/java/config/H2Database.java
+++ src/test/java/config/H2Database.java
@@ -4,10 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class H2ConnectionProvider implements ConnectionProvider {
-
-    @Override
-    public Connection getConnection() throws SQLException {
+public class H2Database {
+    public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 H2DatabaseConfig.DB_URL,
                 H2DatabaseConfig.DB_USER,
--- src/test/java/dao/DatabaseTest.java
+++ src/test/java/dao/DatabaseTest.java
@@ -1,5 +1,6 @@
 package dao;
 
+import config.H2Database;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
@@ -7,12 +8,19 @@ import java.sql.Connection;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class DatabaseTest {
+public class DatabaseTest {
 
     @Test
     void getConnection_shouldReturnValidConnection() throws Exception {
-        Connection conn = Database.getConnection();
-        assertNotNull(conn);
-        assertFalse(conn.isClosed());
+        Connection connection = Database.getConnection();
+        assertNotNull(connection);
+        assertFalse(connection.isClosed());
+    }
+
+    @Test
+    void getConnectionForH2_shouldReturnValidConnection() throws Exception {
+        Connection connection = H2Database.getConnection();
+        assertNotNull(connection);
+        assertFalse(connection.isClosed());
     }
 }
